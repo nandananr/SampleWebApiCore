38e8b6ddd254af8c14a3245d8699105f1e3474a8
nanalapareddy <nanalapareddy@V01PRD00411>
2017-07-05 05:12:04 -0500

    changes in the controller

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -13,7 +13,7 @@ namespace SampleWebAPICore.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2", "value3", "value4" };
+            return new string[] { "value01", "value02" };
         }
 
         // GET api/values/5
